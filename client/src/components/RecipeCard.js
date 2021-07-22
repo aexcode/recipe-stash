368092b8f3a57a209071d59b14870ac4fa4f1775
Ayla Ex <aylaex@protonmail.com>
2021-07-21 20:08:35 -0400

    add initial edit form

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,3 +1,56 @@
-export const RecipeCard = () => {
-  return <h1>Recipe Card</h1>
+import { useState, useRef } from 'react'
+import pencilImg from '../assets/bx-pencil.svg'
+
+export const RecipeCard = ({ recipe }) => {
+  const [isEditing, setIsEditing] = useState(false)
+  const titleRef = useRef()
+  const descriptionRef = useRef()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log({
+      title: titleRef.current.value,
+      description: descriptionRef.current.value,
+    })
+  }
+  return (
+    <div>
+      <button
+        aria-label='Edit this recipe'
+        onClick={() => setIsEditing(!isEditing)}>
+        <img src={pencilImg} alt='' />
+      </button>
+
+      {!isEditing && (
+        <div>
+          <h2>{recipe.title}</h2>
+          <p>{recipe.description}</p>
+        </div>
+      )}
+
+      {isEditing && (
+        <>
+          <button type='submit' onClick={handleSubmit}>
+            Save Changes
+          </button>
+
+          <button type='button' onClick={() => setIsEditing(false)}>
+            Delete Recipe
+          </button>
+
+          <input
+            type='text'
+            aria-label='Edit recipe title'
+            defaultValue={recipe.title}
+            ref={titleRef}
+          />
+
+          <textarea
+            aria-label='Edit recipe description'
+            ref={descriptionRef}
+            defaultValue={recipe.description}></textarea>
+        </>
+      )}
+    </div>
+  )
 }
