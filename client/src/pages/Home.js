9b9bbf17a92c19ace5753bb0caffbb71a02f1bff
Ayla Ex <aylaex@protonmail.com>
2021-07-24 17:41:39 -0400

    initial landing page layout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,69 @@
+import { Link } from 'react-router-dom'
+
 import Layout from '../layout/Layout'
+import image from '../assets/undraw_Notes_re_pxhw.svg'
 
 export const Home = () => {
   return (
     <Layout>
-      <h1>Home</h1>
+      <div className='container'>
+        <div className='d-flex flex-column'>
+          <div className='row flex-md-row-reverse'>
+            {/* image */}
+            <div className='col-md-6'>
+              <img src={image} alt='' className='img-fluid' />
+            </div>
+
+            {/* info */}
+            <div className='col-md-6'>
+              <div className='row '>
+                <h1 className='display-3 text-center text-md-start'>
+                  Recipe Stash
+                </h1>
+                <p className='lead'>
+                  Quick save recipes from around the web, for easy access when
+                  it's time to cook.
+                </p>
+
+                <div class='btn-group-vertical'>
+                  <Link to='/sign-up' class='btn btn-primary btn-lg'>
+                    Get Started
+                  </Link>
+                  <Link to='/sign-in' class='btn btn-outline-primary btn-lg'>
+                    Sign In
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className='container-fluid'>
+        <div className='row'>
+          <div className='col-md-6'>
+            <img src={image} alt='' className='img-fluid' />
+          </div>
+          <div className='col-md-6'>
+            <h1 className='display-3 text-center'>Recipe Stash</h1>
+            <p className='lead'>
+              Quick save recipes from around the web, for easy access when it's
+              time to cook.
+            </p>
+          </div>
+        </div>
+
+        <div className='row'>
+          <div class='btn-group-vertical'>
+            <Link to='/sign-up' class='btn btn-primary btn-lg'>
+              Get Started
+            </Link>
+            <Link to='/sign-in' class='btn btn-outline-primary btn-lg'>
+              Sign In
+            </Link>
+          </div>
+        </div>
+      </div> */}
     </Layout>
   )
 }
